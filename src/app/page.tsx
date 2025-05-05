9bb00b004cee79f3398e7ab19f656efe2da65515
Andrew Njoo <adnjoo@gmail.com>
2025-05-05 05:58:54 -0700

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,8 +41,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <Footer />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="mt-20 text-md text-gray-400 text-center sm:text-left">
-      © {new Date().getFullYear()} StackToSale. Built with focus.
+    <footer className="my-20 text-md text-gray-400 text-center w-full">
+      <div className="max-w-screen-lg mx-auto px-4">
+        © {new Date().getFullYear()} StackToSale. Built with focus.
+      </div>
     </footer>
   );
 }
