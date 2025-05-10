0ca3ed47156287505ff9fd1f0e5a30c65fa328a4
Andrew Njoo <adnjoo@gmail.com>
2025-05-10 12:14:44 -0500

    ga4 (#49)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
+
+const gaId = process.env.NEXT_PUBLIC_GA_ID;
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +38,7 @@ export default function RootLayout({
 
         <Footer />
       </body>
+      {gaId && <GoogleAnalytics gaId={gaId} />}
     </html>
   );
 }
